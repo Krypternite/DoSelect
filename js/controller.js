d2a78818f1d35a56cad23ffdbe942d8b49adde26
Krypternite <kryptonian1111@gmail.com>
2017-08-06 13:42:03 +0530

    Add files via upload
    06/08/2017

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,226 @@
 angular.module('doSelectApp.controllers', [])
+    .filter('labelFilter', function () {
+        return function (items, filterObject) {
+            var filteredRows = items,
+                authorFilter = false,
+                labelFilter = false;
+            if (filterObject.author.length > 0) {
+                authorFilter = true;
+                var temp = [];
+                angular.forEach(items, function (item) {
+                    if (item.author.indexOf(filterObject.author) > -1) {
+                        temp.push(item);
+                    }
+                })
+                filteredRows = temp;
+            } else {
+                authorFilter = false;
+            }
 
-.controller('issueListCtrl', function ($scope) {
-    console.log("CONTROLLER");
-})
+            if (filterObject.labels.length > 0) {
+                labelFilter = true;
+                var labelFilterDataSet = authorFilter == true ? filteredRows : items;
+                var temp = [];
+                angular.forEach(labelFilterDataSet, function (item) {
+                    for (var a = 0; a < item.labels.length; a++) {
+                        if (filterObject.labels.filter(function (e) {
+                                return e.code == item.labels[a].code
+                            }).length > 0) {
+
+                            if (temp.filter(function (e) {
+                                    return e.serial == item.serial
+                                }).length <= 0)
+                                temp.push(item);
+                        }
+                    }
+                });
+                filteredRows = temp;
+
+            } else {
+                labelFilter = false;
+            }
+            return filteredRows;
+        }
+    })
+    .controller('issueListCtrl', function ($scope, issueService, $timeout) {
+        var issueConfigData = {
+            getOpenIssues: function () {
+                issueService.openDatabase().then(function () {
+                    issueService.getOpenIssues().then(function (openIssueList) {
+                        console.log(openIssueList.length);
+                        console.log(openIssueList);
+                        if (openIssueList.length) {
+                            openIssueList.forEach(function (item) {
+                                if ($scope.issueScopeData.authorsList.indexOf(item.author) < 0) {
+                                    $scope.issueScopeData.authorsList.push(item.author);
+                                }
+                                $scope.issueScopeData.openIssueList.push(item);
+
+                            })
+                        }
+
+                    }, function (error) {
+                        console.log(error);
+                    })
+                }, function (err) {
+                    console.log(err)
+                });
+            },
+            getClosedIssues: function () {
+                issueService.openDatabase().then(function () {
+                    issueService.getClosedIssues().then(function (closedIssueList) {
+                        if (closedIssueList.length) {
+                            closedIssueList.forEach(function (item) {
+                                $scope.issueScopeData.closedIssueList.push(item);
+                            })
+                        }
+                    }, function (error) {
+                        console.log(error);
+                    })
+                }, function (err) {
+                    console.log(err)
+                });
+            }
+        };
+        $scope.author = false;
+        $scope.issueScopeData = {
+            filterModel: 'is:issue is:open',
+
+            openIssueList: [],
+            closedIssueList: [],
+            authorsList: [],
+            contains: function (datasetArray, filterObject, type) {
+                var i, property = "";
+                switch (type) {
+                    case 'label':
+                        property = "code";
+                        break;
+                }
+                for (i = 0; i < datasetArray.length; i++) {
+                    if (datasetArray[i][property] === filterObject[property]) {
+                        return true;
+                    }
+                }
+
+                return false;
+            },
+            filterModelUpdate: function (value, type) {
+                if (type === 'author') {
+                    if (value.length > 0)
+                        this.filterModel = this.filterModel.split(/ author:\w+/).join('') + " author:" + value;
+                    else
+                        this.filterModel = this.filterModel.split(/ author:\w+/).join('');
+                    this.filterModel = temp.concat(" author:" + value);
+                } else if (type === 'label') {
+                    var temp = "";
+                    if (this.filterModel.indexOf("label:" + value) < 0) {
+                        temp = this.filterModel.split(' label:' + value).join('') + " label:" + value;
+                    } else {
+                        temp = this.filterModel.split(' label:' + value).join('');
+                    }
+                    this.filterModel = temp;
+                } else if (type === 'sort') {
+                    var temp = this.filterModel.split(' sort:' + value).join('') + " sort:" + value;
+                    this.filterModel = temp;
+                }
+
+            },
+            sortList: angular.copy(window.sortList),
+            sortSelect: function (sortObj) {
+                /* switch (sortType) {
+                     case 'nw':
+                         this.issueFilters.sort = {
+                             sortParam: '-date',
+                             code: sortType
+                         }
+                         break;
+                     case 'ol':
+                         this.issueFilters.sort = {
+                             sortParam: 'date',
+                             code: sortType
+                         }
+                         break;
+                     case 'mc':
+                         this.issueFilters.sort = {
+                             sortParam: '-comments.length',
+                             code: sortType
+                         }
+                         break;
+                     case 'lc':
+                         this.issueFilters.sort = {
+                             sortParam: 'comments.length',
+                             code: sortType
+                         }
+                         break;
+                     case 'ru':
+                         this.issueFilters.sort = {
+                             sortParam: '-updDate',
+                             code: sortType
+                         }
+                         break;
+                     case 'lru':
+                         this.issueFilters.sort = {
+                             sortParam: 'updDate',
+                             code: sortType
+                         }
+                         break;
+                 }*/
+                this.issueFilters.sort = angular.copy(sortObj);
+                this.filterModelUpdate(sortObj.desc, 'sort')
+                /*var temp = this.filterModel.split(' sort:')[0];
+
+this.filterModel = temp.concat(" sort:" + sortObj.desc);*/
+            },
+            authorSelect: function (author) {
+                this.issueFilters.authorFilterModel = '';
+                this.issueFilters.author = this.issueFilters.author === author ? '' : author;
+                this.filterModelUpdate(this.issueFilters.author, 'author')
+            },
+            labelSelect: function (label) {
+                this.issueFilters.labelFilterModel = '';
+                if (this.issueFilters.labels.filter(function (e) {
+                        return e.code == label.code
+                    }).length <= 0) {
+                    this.issueFilters.labels.push(label);
+                } else {
+                    this.issueFilters.labels = this.issueFilters.labels.filter(function (labelObj) {
+                        return labelObj.code !== label.code;
+                    })
+                }
+                this.filterModelUpdate(label.title, 'label')
+            },
+            /*     labelFilter: function () {
+         return function (item) {
+             var result = 0;
+             for (var a = 0; a < item.labels.length; a++) {
+                 if (this.issueFilters.labels.filter(function (e) e.code == label.code).length < 0) {
+                     result = 1;
+                 } else {
+                     result = 0;
+                 }
+             }
+
+         }
+     },*/
+            issueFilters: {
+                authorFilterModel: '',
+                labelFilterModel: '',
+                author: '',
+                labels: [],
+                sort: window.sortList[0],
+            },
+            labelsList: angular.copy(labelArr),
+            toggleMenu: function (menuName) {
+                switch (menuName) {
+                    case 'author':
+
+                        $scope.author = !$scope.author;
+                        break;
+                }
+            }
+        };
+        console.log("CONTROLLER");
+
+        issueConfigData.getOpenIssues();
+        issueConfigData.getClosedIssues()
+    })
--- js/services.js
+++ js/services.js
@@ -1,6 +1,160 @@
+var db, setUp = false,
+    shouldInit = false;
 angular.module('doSelectApp.services', [])
-    .factory('issueService', function () {
+    .factory('issueService', function ($q) {
         return {
+            openDatabase: function () {
+                return $q(function (resolve, reject) {
+                    if (setUp) {
+                        resolve(true);
+                    }
 
+                    var openRequest = window.indexedDB.open("DSIDB", 1);
+
+                    openRequest.onerror = function (e) {
+                        console.log("Error opening db");
+                        reject({
+                            State: 'false',
+                            Reason: e.toString(),
+                            Statement: 'The Db could not be opened.'
+                        });
+                    };
+
+                    openRequest.onupgradeneeded = function (e) {
+
+                        var thisDb = e.target.result;
+                        var objectStore;
+                        shouldInit = true;
+                        //Create ObjectStores For Issues 
+
+                        if (!thisDb.objectStoreNames.contains("openIssues")) {
+                            objectStore = thisDb.createObjectStore("openIssues", {
+                                keyPath: "id",
+                                autoIncrement: true
+                            });
+                            objectStore.createIndex("openIssueTitle", "title", {
+                                unique: false
+                            });
+
+                        }
+
+                        if (!thisDb.objectStoreNames.contains("closedIssues")) {
+                            objectStore = thisDb.createObjectStore("closedIssues", {
+                                keyPath: "id",
+                                autoIncrement: true
+                            });
+                            objectStore.createIndex("closedIssueTitle", "title", {
+                                unique: false
+                            });
+
+
+                        }
+
+
+                    };
+
+                    openRequest.onsuccess = function (e) {
+                        db = e.target.result;
+                        db.onerror = function (event) {
+                            // Generic error handler for all errors targeted at this database's
+                            // requests!
+                            reject("Database error: " + event.target.errorCode);
+                        };
+
+                        setUp = true;
+                        resolve(true);
+                    }
+                });
+
+            },
+            setupIndexedDb: function (parameters) {
+                return $q(function (resolve, reject) {
+
+                    if (shouldInit) {
+                        var transaction, store;
+                        transaction = db.transaction(["openIssues"], "readwrite");
+                        store = transaction.objectStore("openIssues");
+
+                        setupJSON.openIssues.forEach(function (item) {
+                            console.log(item);
+                            var request = store.add(item);
+
+                            request.onerror = function (e) {
+                                console.log("Error", e.target.error.name);
+                                //some type of error handler
+                            }
+
+                            request.onsuccess = function (e) {
+                                console.log("Woot! Did it");
+                            }
+                        });
+
+                        transaction = db.transaction(["closedIssues"], "readwrite");
+                        store = transaction.objectStore("closedIssues");
+
+                        setupJSON.closedIssues.forEach(function (item) {
+                            console.log(item);
+                            var request = store.add(item);
+
+                            request.onerror = function (e) {
+                                console.log("Error", e.target.error.name);
+                                //some type of error handler
+                            }
+
+                            request.onsuccess = function (e) {
+                                console.log("Woot! Did it");
+                            }
+                        });
+                        shouldInit = false;
+                        resolve("SETUP COMPLETE");
+                    } else {
+                        shouldInit = false;
+                        resolve("SETUP NOT NEEDED");
+                    }
+
+                    //Perform the add
+                })
+            },
+            getOpenIssues: function () {
+                var openIssues = [];
+                return $q(function (resolve, reject) {
+                    var transaction = db.transaction(["openIssues"], "readonly");
+                    var objectStore = transaction.objectStore("openIssues");
+
+
+                    objectStore.openCursor().onsuccess = function (e) {
+                        var cursor = e.target.result;
+                        if (cursor) {
+                            openIssues.push(cursor.value);
+                            cursor.continue();
+
+                        }
+                    };
+                    transaction.oncomplete = function (event) {
+                        resolve(openIssues);
+                    };
+                });
+            },
+            getClosedIssues: function () {
+                var closedIssues = [];
+                return $q(function (resolve, reject) {
+
+                    var transaction = db.transaction(["closedIssues"], "readonly");
+                    var objectStore = transaction.objectStore("closedIssues");
+
+
+                    objectStore.openCursor().onsuccess = function (e) {
+                        var cursor = e.target.result;
+                        if (cursor) {
+                            closedIssues.push(cursor.value);
+                            cursor.continue();
+
+                        }
+                    };
+                    transaction.oncomplete = function (event) {
+                        resolve(closedIssues);
+                    };
+                });
+            }
         }
     })
